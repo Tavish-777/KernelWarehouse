199b6091c6ae8c893b2bf86c7eed14556d645d1d
wy <eeshikuangliu@163.com>
2024-11-13 12:05:01 +0800

    div fourier weight

--- modules/kernel_warehouse.py
+++ modules/kernel_warehouse.py
@@ -134,7 +134,7 @@ class KWconvNd(nn.Module):
         for idx in range(len(self.kernel_size)):
             self.groups_spatial = self.groups_spatial * self.kernel_size[idx] // self.cell_shape[3 + idx]
         num_local_mixture = self.groups_out_channel * self.groups_in_channel * self.groups_spatial
-        self.attention = Attention(self.in_planes, reduction, self.cell_shape[0], num_local_mixture,
+        self.attention = Attention(self.in_planes, reduction, self.norm_cell_num, num_local_mixture,
                                    norm_layer=norm_layer, nonlocal_basis_ratio=nonlocal_basis_ratio,
                                    cell_num_ratio=cell_num_ratio, start_cell_idx=start_cell_idx)
         return self.attention.init_temperature(start_cell_idx, cell_num_ratio)
@@ -146,8 +146,8 @@ class KWconvNd(nn.Module):
         batch_size = x.shape[0]
         x = x.reshape(1, -1, *x.shape[2:])
         weight = self.warehouse_manager[0].take_cell(self.warehouse_id).reshape(self.cell_shape[0], -1)
-        weight = weight[:(self.cell_shape[0] - self.num_F),:]
-        F_weight = weight[-self.num_F:,:]
+        # weight = weight[:(self.cell_shape[0] - self.num_F),:]
+        # F_weight = weight[-self.num_F:,:]
         # weight = self.Fourier(self.cell_shape[0],self.cell_shape[1] * self.cell_shape[2]).cuda()
         aggregate_weight = torch.mm(kw_attention, weight)
 
@@ -158,23 +158,27 @@ class KWconvNd(nn.Module):
         output = self.func_conv(x, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
                                 dilation=self.dilation, groups=self.groups * batch_size)
         output = output.view(batch_size, self.out_planes, *output.shape[2:])
-        F_weight = torch.fft.fft2(F_weight)
-        Fkw_attention = torch.fft.fft2(Fkw_attention)
-        F_aggregate_weight = torch.mm(Fkw_attention, F_weight)
-
-        F_aggregate_weight = F_aggregate_weight.reshape([batch_size, self.groups_spatial, self.groups_out_channel,
-                                                     self.groups_in_channel, *self.cell_shape[1:]])
-        F_aggregate_weight = F_aggregate_weight.permute(*self.permute)
-        F_aggregate_weight = F_aggregate_weight.reshape(-1, self.in_planes // self.groups, *self.kernel_size)
-        F_x = torch.fft.fft2(x.to(torch.float32))
-        F_output = self.func_conv(F_x, weight=F_aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
-                                dilation=self.dilation, groups=self.groups * batch_size)
-        F_output = torch.fft.ifft2(F_output).real
-        F_output = F_output.view(batch_size, self.out_planes, *output.shape[2:])
+        x_fft = torch.fft.fft2(x)  # 对输入x进行傅里叶变换
+        weight_fft = torch.fft.fft2(Fkw_attention, s=x.shape[2:])
+        output_fft = x_fft * weight_fft
+        output_fft = torch.fft.ifft2(output_fft).real  # 进行逆傅里叶变换，得到空间域的输出
+        # F_weight = torch.fft.fft2(F_weight)
+        # Fkw_attention = torch.fft.fft2(Fkw_attention)
+        # F_aggregate_weight = torch.mm(Fkw_attention, F_weight)
+        #
+        # F_aggregate_weight = F_aggregate_weight.reshape([batch_size, self.groups_spatial, self.groups_out_channel,
+        #                                              self.groups_in_channel, *self.cell_shape[1:]])
+        # F_aggregate_weight = F_aggregate_weight.permute(*self.permute)
+        # F_aggregate_weight = F_aggregate_weight.reshape(-1, self.in_planes // self.groups, *self.kernel_size)
+        # F_x = torch.fft.fft2(x.to(torch.float32))
+        # F_output = self.func_conv(F_x, weight=F_aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
+        #                         dilation=self.dilation, groups=self.groups * batch_size)
+        # F_output = torch.fft.ifft2(F_output).real
+        # F_output = F_output.view(batch_size, self.out_planes, *output.shape[2:])
         if self.bias is not None:
             output = output + self.bias.reshape(1, -1, *([1]*self.dimension))
-            F_output = F_output + self.bias.reshape(1, -1, *([1] * self.dimension))
-        output = output + F_output
+            # F_output = F_output + self.bias.reshape(1, -1, *([1] * self.dimension))
+        output = output +output_fft
         return output
 
 
